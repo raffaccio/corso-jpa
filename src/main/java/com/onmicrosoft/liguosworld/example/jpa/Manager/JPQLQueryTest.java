1f5a6e05c28d4aab89c19ea23653204d7fcef1a2
raffaccio <qa21882@iperbole.bologna.it>
2025-06-28 11:02:00 +0200

    aggiornamento del pom.xml per l'utilizzo di JPA e Hibernate // Aggiunta di JPQLQueryTest e Main per testare le funzionalità JPA // Aggiunta di persistence.xml per configurare la persistenza

---
+++ src/main/java/com/onmicrosoft/liguosworld/example/jpa/Manager/JPQLQueryTest.java
@@ -0,0 +1,62 @@
+package com.onmicrosoft.liguosworld.example.jpa.Manager;
+
+// This file is part of the course on JPA (Java Persistence API) using Hibernate.
+// It is designed to be used with a MySQL database.
+import com.onmicrosoft.liguosworld.example.jpa.model.Libro;
+import com.onmicrosoft.liguosworld.example.jpa.model.Autore;
+import com.onmicrosoft.liguosworld.example.jpa.model.Editore;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+import java.util.List;
+
+public class JPQLQueryTest {
+    public static void main(String[] args) {
+        // Create an EntityManagerFactory for the persistence unit "DefaultPersistenceUnit"
+        EntityManagerFactory emf = 
+        Persistence.createEntityManagerFactory("DefaultPersistenceUnit");
+        
+        // Create an EntityManager from the EntityManagerFactory
+        EntityManager em = emf.createEntityManager();
+        
+        // Start a transaction
+        em.getTransaction().begin();
+
+        TypedQuery<Libro> query = em.createQuery("SELECT l FROM Libro l", Libro.class);
+        // Execute the query and get the result list
+        List<Libro> libri = query.getResultList();
+        // Check if the result list is not empty
+
+        // Print the results
+        for (Libro libro : libri) {
+            System.out.println("Libro: " + libro.getTitolo() + ", ISBN: " + libro.getIsbn() +
+                               ", Autore: " + libro.getAutore().getNome() + " " + libro.getAutore().getCognome() +
+                               ", Editore: " + libro.getEditore().getNome());
+        }
+        
+        
+        // Create instances of Autore and Editore
+        Autore autore = new Autore("Agatha", "Christie");
+        Editore editore = new Editore("Oscar Mondadori", "Via Arnoldo Mondadori 1");
+        
+        // Create a Libro instance and associate it with the Autore and Editore
+        Libro libro = new Libro("Assassinio sull'Orient Express", "978-8804732532", autore, editore);
+        
+        // Persist the entities to the database
+        em.persist(autore);
+        em.persist(editore);
+        em.persist(libro);
+        
+        // Commit the transaction
+        em.getTransaction().commit();
+        
+        // Close the EntityManager
+        em.close();
+        
+        // Close the EntityManagerFactory
+        emf.close();
+    }
+    
+}
---
+++ src/main/java/com/onmicrosoft/liguosworld/example/jpa/Manager/Main.java
@@ -0,0 +1,47 @@
+package com.onmicrosoft.liguosworld.example.jpa.Manager;
+
+// This file is part of the course on JPA (Java Persistence API) using Hibernate.
+// It is designed to be used with a MySQL database.
+import com.onmicrosoft.liguosworld.example.jpa.model.Libro;
+import com.onmicrosoft.liguosworld.example.jpa.model.Autore;
+import com.onmicrosoft.liguosworld.example.jpa.model.Editore;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create an EntityManagerFactory for the persistence unit "corso_jpa"
+        EntityManagerFactory emf = 
+        Persistence.createEntityManagerFactory("DefaultPersistenceUnit");
+        
+        // Create an EntityManager from the EntityManagerFactory
+        EntityManager em = emf.createEntityManager();
+        
+        // Start a transaction
+        em.getTransaction().begin();
+        
+        // Create instances of Autore and Editore
+        Autore autore = new Autore("Agatha", "Christie");
+        Editore editore = new Editore("Oscar Mondadori", "Via Arnoldo Mondadori 1");
+        
+        // Create a Libro instance and associate it with the Autore and Editore
+        Libro libro = new Libro("Assassinio sull'Orient Express", "978-8804732532", autore, editore);
+        
+        // Persist the entities to the database
+        em.persist(autore);
+        em.persist(editore);
+        em.persist(libro);
+        
+        // Commit the transaction
+        em.getTransaction().commit();
+        
+        // Close the EntityManager
+        em.close();
+        
+        // Close the EntityManagerFactory
+        emf.close();
+    }
+    
+}
